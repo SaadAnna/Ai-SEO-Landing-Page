6b9b226085fe642372a1cf8f44f9252c291f7b16
saad <saadannadev@gmail.com>
2025-01-31 08:29:39 +0100

    Finish The Header Section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,21 +3,21 @@ import MyMenuIcon from "@/assets/icon-menu.svg";
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/15">
+    <header className="py-4 border-b border-white/15 md:border-none">
       <div className="container">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl lg:max-w-3xl mx-auto">
           <div>
             <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
               <LogoIcon className="h-8 w-8" />
             </div>
           </div>
-          <div>
-          <nav>
+          <div className="hidden md:block">
+          <nav className="flex gap-8 text-sm">
 
-          <a href="#">Features</a>
-          <a href="#">Developers</a>
-          <a href="#">Pricing</a>
-          <a href="#">ChnageLog</a>
+          <a href="#" className="text-white/70 hover:text-white transition">Features</a>
+          <a href="#" className="text-white/70 hover:text-white transition">Developers</a>
+          <a href="#" className="text-white/70 hover:text-white transition">Pricing</a>
+          <a href="#" className="text-white/70 hover:text-white transition">ChnageLog</a>
           </nav>
 
           </div>
@@ -30,7 +30,7 @@ export const Header = () => {
               </div>
               <span>join waitlist</span>
             </button>
-            <MyMenuIcon />
+            <MyMenuIcon className="md:hidden" />
           </div>
         </div>
       </div>
