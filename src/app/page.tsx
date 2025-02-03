721c6d305e1ad4da5feb4801e60a95ec89e60443
saad <saadannadev@gmail.com>
2025-02-03 13:57:54 +0100

    Add the features section to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
-import { CallToAction } from '@/sections/CallToAction';
+import { Features } from '@/sections/Features';
 export default function Home() {
   return(
   <>
   <Header />
   <Hero />
   <LogoTicker /> 
-  <CallToAction /> 
+  <Features /> 
   </>
   ) ;
 }
