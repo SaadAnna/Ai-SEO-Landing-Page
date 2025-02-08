ab4d1652bbcad04939af458766a7c80dbc442f80
saad <saadannadev@gmail.com>
2025-02-08 16:25:21 +0100

    Add framer animation to the logo ticker section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,9 +1,11 @@
+'use client';
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
 import clelestialLogo from "@/assets/logo-celestial.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import echoLogo from "@/assets/logo-echo.png";
+import { motion } from "framer-motion";
 export const LogoTicker = () => {
   return (
     <section className="py-20 md:py-24">
@@ -12,8 +14,18 @@ export const LogoTicker = () => {
         <div className="flex-1 md:flex-none">
           <h2>Trusted by top innovative teams</h2>
           </div>
-          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex flex-none gap-14 ">
+          <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+           initial={{translateX: "-50%"}}
+           animate={{translateX: '0'}}
+           transition={{
+            repeat: Infinity,
+            duration: 30,
+            ease: "linear",
+           }}
+          
+          
+          className="flex flex-none gap-14 pr-14 -translate-x-1/2 ">
             {[
               acmeLogo,
               apexLogo,
@@ -21,10 +33,17 @@ export const LogoTicker = () => {
               quantumLogo,
               pulseLogo,
               echoLogo,
+              acmeLogo,
+              apexLogo,
+              clelestialLogo,
+              quantumLogo,
+              pulseLogo,
+              echoLogo,
             ].map((logo) => (
+              // eslint-disable-next-line @next/next/no-img-element
               <img src={logo.src} key={logo.src} className="h-6 w-auto" />
             ))}
-          </div>
+          </motion.div>
           </div>
         </div>
       </div>
