9b6519be706da6d556b99699f3eb0ef10f59ca6a
saad <saadannadev@gmail.com>
2025-02-09 23:31:09 +0100

    Start add the framer motion to the features section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -35,7 +35,7 @@ export const Features = () => {
       <motion.div className="container">
         <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">
           {" "}
-          Elevate your SEO efforts
+          Elevate your SEO efforts 
         </h2>
         <p className="text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5">
           From small startups to large enterprises, Our AI-drives toll has
