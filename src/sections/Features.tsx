e998200d66bc6e0a16043e381d2de9dd7935c2b9
saad <saadannadev@gmail.com>
2025-02-10 21:29:34 +0100

    Finish the framer animation in the features section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,7 +1,17 @@
 "use client";
 import ProductImage from "@/assets/product-image.png";
-import { DotLottiePlayer } from "@dotlottie/react-player";
-import { motion } from "framer-motion";
+import {
+  DotLottieCommonPlayer,
+  DotLottiePlayer,
+} from "@dotlottie/react-player";
+import {
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  ValueAnimationTransition,
+} from "framer-motion";
+import { use, useEffect, useRef } from "react";
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -28,14 +38,67 @@ const tabs = [
     backgroundSizeX: 177,
   },
 ];
-
+const FeaturesTab = (tab: (typeof tabs)[number]) => {
+  const TabRef = useRef<HTMLDivElement>(null);
+  const dotlottieref = useRef<DotLottieCommonPlayer>(null);
+  const XPercentage = useMotionValue(0);
+  const YPercentage = useMotionValue(0);
+  const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${XPercentage}% ${YPercentage}%,black,transparent)`;
+  useEffect(() => {  
+    animate(XPercentage, [0, 100, 100, 0, 0], {
+       duration: 5,
+      repeat: Infinity,
+      ease: "linear",
+      repeatType: "reverse",
+    });
+    animate(YPercentage, [0, 0, 100, 100, 0], {
+       duration: 5,
+      repeat: Infinity,
+      ease: "linear",
+      repeatType: "reverse",
+    });
+  }, [XPercentage, YPercentage]);
+  const HandleTabHover = () => {
+    if (dotlottieref.current === null) return;
+    dotlottieref.current.seek(0);
+    dotlottieref.current.play();
+  };
+  return (
+    <div className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 relative">
+      <div
+        ref={TabRef}
+        onMouseEnter={HandleTabHover}
+        className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center"
+      >
+        <motion.div
+          style={{
+            maskImage,
+          }}
+          className="absolute inset-0 -m-px rounded-xl border border-[#A369FF] "
+        ></motion.div>
+        <DotLottiePlayer
+          ref={dotlottieref}
+          src={tab.icon}
+          className="h-5 w-5"
+          autoplay
+        />
+      </div>
+      <div className="font-medium">{tab.title}</div>
+      {tab.isNew && (
+        <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
+          new
+        </div>
+      )}
+    </div>
+  );
+};
 export const Features = () => {
   return (
     <section className="py-20 md:py-24">
       <motion.div className="container">
         <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">
           {" "}
-          Elevate your SEO efforts 
+          Elevate your SEO efforts
         </h2>
         <p className="text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5">
           From small startups to large enterprises, Our AI-drives toll has
@@ -43,20 +106,7 @@ export const Features = () => {
         </p>
         <div className="mt-10 flex flex-col lg:flex-row gap-3">
           {tabs.map((tab) => (
-            <div
-              key={tab.title}
-              className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1"
-            >
-              <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
-                <DotLottiePlayer src={tab.icon} className="h-5 w-5" autoplay />
-              </div>
-              <div className="font-medium">{tab.title}</div>
-              {tab.isNew && (
-                <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
-                  new
-                </div>
-              )}
-            </div>
+            <FeaturesTab {...tab} key={tab.title} />
           ))}
         </div>
         <div className="border border-white/20 p-2.5 rounded-xl mt-3 ">
